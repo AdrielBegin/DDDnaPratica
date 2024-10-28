b0d17b2be4d2cc0d36c3be62cad8520d3909b99e
Adriel Pereira <adriel.pereira@agendaassessoria.com.br>
2024-10-28 17:11:07 -0400

    feat: melhorando o token

--- BuberDinner/BuberDinner.Api/obj/Debug/net8.0/BuberDinner.Api.AssemblyInfo.cs
+++ BuberDinner/BuberDinner.Api/obj/Debug/net8.0/BuberDinner.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberDinner.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83de13d74bb1d03fea4b45a983a93f2115049f22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7417daafb0ed777b9cc4cc661261084dbf0f2766")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberDinner.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberDinner.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BuberDinner/BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ BuberDinner/BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -2,18 +2,28 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using BuberDinner.Application.Common.Interfarces.Authentication;
+using BuberDinner.Application.Common.Interfarces.Services;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 namespace BuberDinner.Infrastructure.Authentication;
 
 public class JwtTokenGenerator : IJwtTokenGenerator
 {
+    private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly JwtSettings _jwtSettings;
+
+    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider, IOptions<JwtSettings> jwtOptions)
+    {
+        _dateTimeProvider = dateTimeProvider;
+        _jwtSettings = jwtOptions.Value;
+    }
     public string GenerateToken(Guid userId, string firstName, string lastName)
     {
 
         var signingCredentials = new SigningCredentials(
             new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes("your-super-secret-longer-key-32-characters-minimum")),
+                Encoding.UTF8.GetBytes(_jwtSettings.Secret)),
                 SecurityAlgorithms.HmacSha256
             );        
 
@@ -26,8 +36,9 @@ public class JwtTokenGenerator : IJwtTokenGenerator
         };
 
          var securityToken = new JwtSecurityToken(
-            issuer: "BuberDinner",
-            expires: DateTime.Now.AddDays(1),
+            issuer: _jwtSettings.Issuer,
+            audience:_jwtSettings.Audience,
+            expires: _dateTimeProvider.UtcNow.AddMinutes(60),
             claims: claim,
             signingCredentials: signingCredentials
          );
